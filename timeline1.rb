ef0ef7caacabc68c3562249e229596edae4fe06a
Pair <ken@seeice.com>
2013-09-14 13:56:37 +0000

    added two timeline

---
+++ timeline1.rb
@@ -0,0 +1,11 @@
+require 'twitter'
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "HdvvOP7MKzMsQErS6CurXA"
+  config.consumer_secret     = "VPRYvu82UA26WWL5t6jo2KyKjw2TZ1SpJENzeiE"
+  config.access_token        = "1863636288-Ecsxgw5zncRVTuLoPRLDdx5ApPcuSGrcoBxNnHP"
+  config.access_token_secret = "maQDDGGyZ0xaXlOx2liLmzAag2KqUWqwrdQq3imsDuQ"
+end
+
+client.user do |tweet|
+  puts tweet.text
+end
